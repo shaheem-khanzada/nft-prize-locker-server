46a8c4492acc962ceba99d85a533ae727f7d631e
Shaheem Khan <mshaheemkz@gmail.com>
2022-06-28 20:18:35 +0500

    fix gitignore

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
     transform: true,
     enableDebugMessages: true,
   }));
-  console.log('process.env.PORT', process.env.PORT);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(4000);
 }
 bootstrap();
